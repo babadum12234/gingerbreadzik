cd11be49247fe90173e7b9208accc9073a081e96
babadum12234 <celej.piotrek@gmail.com>
2023-11-28 10:25:38 +0100

    endofprimarycode!

---
+++ caIculator.py
@@ -0,0 +1,4 @@
+print("podaj liczbe:") 
+a=input()
+
+
--- primary.py
+++ primary.py
@@ -1,22 +1,29 @@
 primary=3
 tab_primarys=[3]
 j=0
-#tab_primarys.append(3)
-for i in range(100):
+print("co ktora liczbe pierwsza chcesz dostac:")
+a=int(input())
+def wypisanie(h, a):
+    if h%a == 0:
+        print(primary)
+h=0
+print("2")
+if a == 1:
+    print("3")
+while h==h:
+    h+=1
     j=0
     primary=primary+2
     while tab_primarys[j]<1/2 * primary:
         if primary%tab_primarys[j]==0:
             primary=primary+2
+            j=0
         else:
             j=j+1
-            if j>len(tab_primarys):
+            if j+1>len(tab_primarys):
                 primary=primary+2
+                j=0
 
     tab_primarys.append(primary)
-    print(primary)
-
-#i:                   0 1   
-#f:                   0 1
-#primary:           3 5
-#tab_primary:       3
+    # print(primary)
+    wypisanie(h,a)
